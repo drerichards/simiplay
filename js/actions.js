4dc70a18e94c693be2f44f6aeca0f68e8b7773b6
Andre <elij.richards22@gmail.com>
2016-06-16 03:24:14 -0400

    simi art loads commit

--- js/actions.js
+++ js/actions.js
@@ -1,6 +1,7 @@
 var searchButton = document.getElementById('searchButton');
 var searchResults = function(searchValue) {
   $('.artistMainPic img').remove();
+  $('#tracksContainer').css("visibility","hidden");
   var params = {
       q: searchValue,
       type: 'artist',
@@ -19,17 +20,45 @@ var searchResults = function(searchValue) {
       event.preventDefault();
       var index = $(this).index();
       var artistID = artistObjects[index].id;
-      var similarArtURL = 'https://api.spotify.com/v1/artists/'+artistID+'/related-artists';
-      $('li').remove();
-      $('.artistMainPic').html('<img src="'+artistObjects[index].images[0].url+'" alt="" />');
+      var simiArtURL = 'https://api.spotify.com/v1/artists/'+artistID+'/related-artists';
+      $('li').toggle(600);
+      $('.artistMainPic').html('<img src="'+artistObjects[index].images[1].url+'" />');
 
-      $.getJSON(similarArtURL, function(data) {
+      $.getJSON(simiArtURL, function(data) {
           console.log(data);
-          var similarArtData = data.artists;
-          for (var i = 0; i < similarArtData.length; i++) {
-            var dataItems = similarArtData[i].name;
-            $('.thumbnailsList').append('<li>'+dataItems+'</li>');
+          var simiArtData = data.artists;
+          for (var i = 0; i < 10; i++) {
+            var simiArtPic = simiArtData[i].images[1].url;
+            $('.thumbnailsList').append('<li><img src="'+simiArtPic+'" /></li>');
           }
+          $('li').click(function(event) {
+            event.preventDefault();
+            var index = $(this).index();
+            var simiArtID = simiArtData[index].id;
+            var tracksTitle = $('.tracksTitle');
+            $('#tracksContainer').css("visibility","visible");
+            $('.trackList li').remove();
+            $('.songPic img').remove();
+            tracksTitle.val('');
+            tracksTitle.html('Top 10 Tracks for: <strong>'+simiArtData[index].name+'</strong>');
+            var topTracksURL = 'https://api.spotify.com/v1/artists/'+simiArtID+'/top-tracks?country=SE'
+
+              $.getJSON(topTracksURL, function(data) {
+                console.log(data);
+                var topTrackData = data.tracks;
+                for (var i = 0; i < topTrackData.length; i++) {
+                  var track = topTrackData[i].name;
+                  $('.trackList').append('<li><div class="songBar">'+track+'</div></li>');
+                }
+                $('.songPic').html('<img src="'+topTrackData[0].album.images[0].url+'" />');
+                
+                $('.trackList li').click(function() {
+                  var index = $(this).index();
+                  $('.songPic img').remove();
+                  $('.songPic').html('<img src="'+topTrackData[index].album.images[0].url+'" />');
+                });
+              });
+          });
       });
     });
   });
