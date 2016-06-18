98b646e337eac542c4e1b2001523dd26fc710951
Andre <elij.richards22@gmail.com>
2016-06-18 14:55:55 -0400

    audio issue commit

--- js/actions.js
+++ js/actions.js
@@ -5,8 +5,8 @@ var searchResults = function(searchValue) {
   var params = {
       q: searchValue,
       type: 'artist',
-      limit: 50
-};
+      limit: 20
+    };
   url = 'https://api.spotify.com/v1/search';
   $.getJSON(url, params, function(data) {
     $('li').remove();
@@ -24,61 +24,79 @@ var searchResults = function(searchValue) {
       $('li').toggle(600);
       $('.artistMainPic').html('<img src="'+artistObjects[index].images[1].url+'" />');
 
+      displayTrackPlayer(artistObjects, index, artistID);
+
       $.getJSON(simiArtURL, function(data) {
           console.log(data);
           var simiArtData = data.artists;
           for (var i = 0; i < simiArtData.length; i++) {
             var simiArtPic = simiArtData[i].images[1].url;
-            $('.thumbnailsList').append('<li><img src="'+simiArtPic+'" /></li>');
+            var simiArtName = simiArtData[i].name;
+
+            $('.thumbnailsList').append('<li><div class="songBar"><img src="'+simiArtPic+'" />'+simiArtName+'</div></li>');
           }
-          $('li').click(function(event) {
+          $('.thumbnailsList li').click(function(event) {
             event.preventDefault();
             var index = $(this).index();
             var simiArtID = simiArtData[index].id;
-            var tracksTitle = $('.tracksTitle');
-            $('#tracksContainer').css("visibility","visible");
-            $('.trackList li').remove();
-            $('.songPic img').remove();
-            tracksTitle.val('');
-            tracksTitle.html('Top 10 Tracks for: <strong>'+simiArtData[index].name+'</strong>');
-            var topTracksURL = 'https://api.spotify.com/v1/artists/'+simiArtID+'/top-tracks?country=SE'
 
-              $.getJSON(topTracksURL, function(data) {
-                console.log(data);
-                var topTrackData = data.tracks;
-                for (var i = 0; i < topTrackData.length; i++) {
-                  var track = topTrackData[i].name;
-                  $('.trackList').append('<li><div class="songBar">'+track+'</div></li>');
-                }
-                var playBtn = ('<input class="play" type="button" value="Play" />');
-                var pauseBtn = ('<input class="pause" type="button" value="Pause" />');
-                $('.songPic').html('<img src="'+topTrackData[0].album.images[0].url+'" />'+playBtn+pauseBtn);
-
-                function playAudio(index) {
-                  var audio = new Audio(topTrackData[index].preview_url);
-                  audio.play();
-
-                  $('li, .pause').click(function (event) {
-                    event.preventDefault();
-                    audio.pause();
-
-                    $('.play').click(function (event) {
-                      event.preventDefault();
-                      audio.play();
-                    });                  
-                  });
-                }
-                playAudio(0);
-
-                $('.trackList li').click(function() {
-                  var index = $(this).index();
-                  $('.songPic img').remove();
-                  $('.songPic').html('<img src="'+topTrackData[index].album.images[0].url+'" />'+playBtn+pauseBtn);
-                  playAudio(index);
-                });
-              });
+            displayTrackPlayer(simiArtData, index, simiArtID);
           });
-      });
+        });
+
+      function displayTrackPlayer(dataObjects, index, id) {
+        var tracksTitle = $('.tracksTitle');
+        $('#tracksContainer').css("visibility","visible");
+        $('.trackList li').remove();
+        $('.songPic img').remove();
+        tracksTitle.val('');
+        tracksTitle.html('Top 10 Tracks for: <strong>'+dataObjects[index].name+'</strong>');
+        var topTracksURL = 'https://api.spotify.com/v1/artists/'+id+'/top-tracks?country=SE'
+
+          $.getJSON(topTracksURL, function(data) {
+            console.log(data);
+            var topTrackData = data.tracks;
+            for (var i = 0; i < topTrackData.length; i++) {
+              var track = topTrackData[i].name;
+              $('.trackList').append('<li><div class="songBar">'+track+'</div></li>');
+            }
+            var playBtn = ('<input class="play" type="image" src="images/play.png" />');
+            var pauseBtn = ('<input class="pause" type="image" src="images/pause.png" />');
+            $('.songPic').html('<img src="'+topTrackData[0].album.images[0].url+'" />'+playBtn+pauseBtn);
+
+            function playAudio(index) {
+              var audio = new Audio(topTrackData[index].preview_url);
+              audio.currentTime = 0;
+              audio.play();
+              $('.pause').click(function (event) {
+                event.preventDefault();
+                audio.pause();
+              });
+              $('.play').click(function (event) {
+                event.preventDefault();
+                audio.play();
+              });
+
+              $('#searchButton').click(function (event) {
+                event.preventDefault();
+                audio.pause();
+                audio.currentTime = 0;
+              });
+              $('li').click(function(event) {
+                event.preventDefault();
+                audio.pause();
+                
+                audio.currentTime = 0;
+                var index = $(this).index();
+                $('.songPic img').remove();
+                $('.songPic').html('<img src="'+topTrackData[index].album.images[0].url+'" />'+playBtn+pauseBtn);
+                playAudio(index);
+              });
+            }
+            playAudio(0);
+
+          });
+        }
     });
   });
 };
